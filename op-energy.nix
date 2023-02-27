f398ea9b9b3c0813824430eec6f53201929d8f35
BuildTools <unconfigured@null.spigotmc.org>
2023-02-27 21:43:03 +0800

    add op-energy test

---
+++ op-energy.nix
@@ -0,0 +1,78 @@
+let
+  # Pin nixpkgs, see pinning tutorial for more details
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/0f8f64b54ed07966b83db2f20c888d5e035012ef.tar.gz";
+  pkgs = import nixpkgs {};
+
+  # Single source of truth for all tutorial constants
+  database      = "postgres";
+  schema        = "api";
+  table         = "todos";
+  username      = "authenticator";
+  password      = "mysecretpassword";
+  webRole       = "web_anon";
+  postgrestPort = 3000;
+
+  # NixOS module shared between server and client
+  sharedModule = {
+    # Since it's common for CI not to have $DISPLAY available, we have to explicitly tell the tests "please don't expect any screen available"
+    virtualisation.graphics = false;
+  };
+
+in pkgs.nixosTest ({
+  # NixOS tests are run inside a virtual machine, and here we specify system of the machine.
+  system = "x86_64-linux";
+
+  nodes = {
+    server = { config, pkgs, ... }: {
+      imports = [
+        sharedModule
+        ./op-energy-development/host.nix
+      ];
+
+      networking.firewall.allowedTCPPorts = [ 8999 ];
+
+      users = {
+        mutableUsers = false;
+        users = {
+          # For ease of debugging the VM as the `root` user
+          root.password = "";
+
+          # Create a system user that matches the database user so that we
+          # can use peer authentication.  The tutorial defines a password,
+          # but it's not necessary.
+          "${username}".isSystemUser = true;
+        };
+      };
+
+    };
+
+    client = {
+      imports = [ sharedModule ];
+    };
+  };
+
+  # Disable linting for simpler debugging of the testScript
+  skipLint = true;
+
+  testScript = ''
+    import json
+    import sys
+
+    start_all()
+
+    server.wait_for_open_port(${toString postgrestPort})
+
+    expected = [
+        {"id": 1, "done": False, "task": "finish tutorial 0", "due": None},
+        {"id": 2, "done": False, "task": "pat self on back", "due": None},
+    ]
+
+    actual = json.loads(
+        client.succeed(
+            "${pkgs.curl}/bin/curl http://server:${toString postgrestPort}/${table}"
+        )
+    )
+
+    assert expected == actual, "table query returns expected content"
+  '';
+})
