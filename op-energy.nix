b38151ece27ea2e5a372fa9b1debbfc776568781
BuildTools <unconfigured@null.spigotmc.org>
2023-02-27 23:56:27 +0800

    Revert "pin nixpkgs to 22.05"
    This reverts commit 9ea56f53ab019001db8e9a837ede4e9152107ece.

--- op-energy.nix
+++ op-energy.nix
@@ -1,6 +1,6 @@
 let
   # Pin nixpkgs, see pinning tutorial for more details
-  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.05.tar.gz";
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/archive/0f8f64b54ed07966b83db2f20c888d5e035012ef.tar.gz";
   pkgs = import nixpkgs {};
 
   # Single source of truth for all tutorial constants
