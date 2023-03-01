77acaa205c3a9d450750e06484a02f3209b82d51
BuildTools <unconfigured@null.spigotmc.org>
2023-03-01 14:42:39 +0800

    fix

--- op-energy.nix
+++ op-energy.nix
@@ -57,11 +57,6 @@ in pkgs.nixosTest ({
         users = {
           # For ease of debugging the VM as the `root` user
           root.password = "";
-
-          # Create a system user that matches the database user so that we
-          # can use peer authentication.  The tutorial defines a password,
-          # but it's not necessary.
-          "${username}".isSystemUser = true;
         };
       };
 
