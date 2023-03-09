78f69ae9e039f3b13d540aaeee7d0eddb23e0fdb
Dambaev Alexander <ice.redmine@gmail.com>
2023-03-09 17:52:26 +0800

    fix test

--- op-energy.nix
+++ op-energy.nix
@@ -77,7 +77,7 @@ in pkgs.nixosTest ({
 
     actual = json.loads(
         client.succeed(
-            "${pkgs.curl}/bin/curl http://server:${toString apiPort}/api/v1/git-hash"
+            "${pkgs.curl}/bin/curl http://server:${toString apiPort}/api/v1/oe/git-hash"
         )
     )
 
